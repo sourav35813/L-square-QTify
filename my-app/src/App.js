066f670f80b96219c98732cc8cd2811ae0dc5134
sourav <sourav35813@gmail.com>
2024-05-30 10:43:58 +0530

    song commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -126,7 +126,7 @@ function App() {
       <div className={styles.sectionWrapper}>
         <Section type='album' title='Top Albums' data={topAlbumSongs} />
         <Section type='album' title='New Albums' data={newAlbumSongs} />
-        {/* <FilterSection data={newAlbumSongs} type='songFilter' title='Songs' filteredData={filteredData} filteredDataValues={filteredDataValues} value={value} handleChange={handleChange} handleToggle={handleToggle}/> */}
+        <FilterSection data={newAlbumSongs} type='songFilter' title='Songs' filteredData={filteredData} filteredDataValues={filteredDataValues} value={value} handleChange={handleChange} handleToggle={handleToggle}/>
       </div>
     </>
   )
